b062f1d0b313b9d827a06872b73d4475c3daa2eb
ninh <kimninh1610@gmail.com>
2021-02-24 17:26:40 +0700

    update app

--- app/src/main/java/yan/intentinspector/MainActivity.kt
+++ app/src/main/java/yan/intentinspector/MainActivity.kt
@@ -1,112 +1,80 @@
-package yan.intentinspector;
+package yan.intentinspector
 
-import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.ExpandableListView;
-import android.widget.SimpleExpandableListAdapter;
-import android.widget.TextView;
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.ExpandableListView
+import android.widget.ExpandableListView.OnChildClickListener
+import android.widget.SimpleExpandableListAdapter
+import androidx.appcompat.app.AppCompatActivity
+import yan.intentinspector.databinding.ActivityMainBinding
+import java.util.*
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+class MainActivity : AppCompatActivity(), OnChildClickListener {
 
+    private lateinit var binding: ActivityMainBinding
 
-public class MainActivity extends ActionBarActivity {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        val groupData: MutableList<Map<String, String>> = ArrayList()
+        val listOfChildGroups: MutableList<List<Map<String, String>>> = ArrayList()
 
-        TextView infoAction = (TextView) findViewById(R.id.textViewAction);
-        TextView infoType = (TextView) findViewById(R.id.textViewType);
-        Intent receivedIntent = getIntent();
-        String receivedInfo = receivedIntent.getAction();
-        if(receivedInfo != null) {
-            infoAction.setText(receivedInfo);
-        }
+        intent.extras?.let { extras ->
+            for (key in extras.keySet()) {
+                val value = extras[key]!!
 
-        receivedInfo = receivedIntent.getType();
-        if(receivedInfo != null) {
-            infoType.setText(receivedInfo);
-        }
-
-        List<Map<String, String>> groupData = new ArrayList<>();
-        List<List<Map<String, String>>> listOfChildGroups = new ArrayList<>();
-
-        Bundle bundle = receivedIntent.getExtras();
-        if(bundle != null) {
-            for (String key : bundle.keySet()) {
-                Object v = bundle.get(key);
-                HashMap<String, String> grp = new HashMap<>();
-                grp.put("ROOT_NAME", String.format("%s (%s)", key , getObjectType(v)));
-                groupData.add(grp);
-                listOfChildGroups.add( populateGroupRow(v));
+                val grp = HashMap<String, String>()
+                grp["EXTRA_KEY"] = key
+                grp["EXTRA_VALUE_TYPE"] = value::class.java.name
+                groupData.add(grp)
+                listOfChildGroups.add(populateGroupRow(value))
             }
         }
+        val adapter = SimpleExpandableListAdapter(this,
+                groupData, android.R.layout.simple_expandable_list_item_2, arrayOf("EXTRA_KEY", "EXTRA_VALUE_TYPE"), intArrayOf(android.R.id.text1, android.R.id.text2),
+                listOfChildGroups, R.layout.my_expandable_list_item, arrayOf("CHILD_NAME"), intArrayOf(android.R.id.text1)
+        )
 
-        SimpleExpandableListAdapter adapter = new SimpleExpandableListAdapter(this,
-
-                groupData, android.R.layout.simple_expandable_list_item_1,
-                new String[] { "ROOT_NAME"}, new int[] {android.R.id.text1},
-
-                listOfChildGroups, android.R.layout.simple_expandable_list_item_2,
-                new String[] { "CHILD_NAME", "CHILD_NAME"}, new int[] {android.R.id.text1, android.R.id.text2}
-            );
-
-        ExpandableListView elv = (ExpandableListView) findViewById(R.id.expandableListView);
-        elv.setAdapter(adapter);
-    }
-
-    private String getObjectType(Object v) {
-        String info = v.getClass().toString();
-        if(info.startsWith("class ")) {
-            info = info.substring(6);
+        with(binding) {
+            textViewAction.text = intent.action
+            textViewType.text = intent.type
+            expandableListView.setOnChildClickListener(this@MainActivity)
+            expandableListView.setAdapter(adapter)
         }
-        return info;
     }
 
-    private List<Map<String, String>> populateGroupRow(Object v){
-        List<Map<String, String>> groupRow = new ArrayList<>();
-        if(v instanceof List) {
-            List<Object> info =  (List<Object>) v;
-            for(Object subv : info) {
-                HashMap<String, String> sub = new HashMap<>();
-                sub.put("CHILD_NAME", subv.toString());
-                groupRow.add(sub);
+    private fun populateGroupRow(v: Any): List<Map<String, String>> {
+        val groupRow: MutableList<Map<String, String>> = ArrayList()
+
+        if (v is List<*>) {
+            val sub = HashMap<String, String>()
+            for (subv in v) {
+                sub["CHILD_NAME"] = subv.toString()
+                groupRow.add(sub)
             }
+        } else {
+            val sub = HashMap<String, String>()
+            sub["CHILD_NAME"] = v.toString()
+            groupRow.add(sub)
         }
-        else {
-            HashMap<String, String> sub = new HashMap<>();
-            sub.put("CHILD_NAME", v.toString());
-            groupRow.add(sub);
-        }
-        return groupRow;
+        return groupRow
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    override fun onChildClick(
+            parent: ExpandableListView,
+            v: View,
+            groupPosition: Int,
+            childPosition: Int,
+            id: Long): Boolean {
+        Log.d(TAG, String.format("groupPosition: %d, childPosition: %d, id: %d",
+                groupPosition, childPosition, id))
+        return true
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    companion object {
+        val TAG: String = MainActivity::class.java.simpleName
     }
 }
